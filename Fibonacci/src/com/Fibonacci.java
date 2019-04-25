7b3a86cdac5e7dbbe48cac50a1a2a94c1a77e9c9
AnneYanggg <38997439+AnneYanggg@users.noreply.github.com>
2019-04-25 10:49:44 +0800

    Fibonacci

---
+++ Fibonacci/src/com/Fibonacci.java
@@ -0,0 +1,30 @@
+package com;
+
+import java.math.BigInteger;
+
+public class Fibonacci {
+	
+	public static BigInteger Fibonacci_of(int count){
+		if(count == 1) {
+			return new BigInteger("1");
+		}
+		
+		BigInteger n1 = new BigInteger("0"), n2 = new BigInteger("1"), n3 = new BigInteger("0");
+		for(int i = 0; i < count-1; i++) {
+			n3 = n1.add(n2);
+			n1 = n2;
+			n2 = n3;
+		}
+		return n3;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 200;
+		for(int i=1; i <= count; i++) {
+			System.out.print("Fibonacci.of(" + i + ") == " + Fibonacci_of(i) + "\n");
+		}
+		
+	}
+
+}
